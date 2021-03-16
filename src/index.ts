dad6a58592f15c345ef7d6eef8fe89653eadc2ef
bkdev98 <bkdev98@gmail.com>
2021-03-16 15:40:39 +0700

    feat(locales): add locale vi, th, ru

--- src/index.ts
+++ src/index.ts
@@ -6,5 +6,8 @@ import * as ar from './locales/ar';
 import * as it from './locales/it';
 import * as es from './locales/es';
 import * as pt from './locales/pt';
+import * as vi from './locales/vi';
+import * as th from './locales/th';
+import * as ru from './locales/ru';
 
-export { fr, nl, nb, de, ar, es, it, pt };
+export { fr, nl, nb, de, ar, es, it, pt, vi, th, ru };
